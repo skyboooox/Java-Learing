eafa622addeb533292710c6a760781137a806f06
zcj924 <zcj924@gmail.com>
2020-02-24 11:51:01 +0800

    Create 1-20交互.java

---
+++ 1-20交互.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class test1_20{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System. out.println("请输入您的姓名: ");
+        String str = scan.next();
+        System. out.println(str+ ",您好，欢迎学习Java! ");
+        scan.close();}
+    
+}
