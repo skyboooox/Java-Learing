0c3b25e301903bb717ce93667f21a928811889fc
zcj924 <zcj924@gmail.com>
2020-03-02 11:14:30 +0800

    Revert "-"
    This reverts commit 3e06c684e8bcaab391fc8602b6bdb48f77ed8977.

--- src/简易计算器/Calculator.java
+++ src/简易计算器/Calculator.java
@@ -4,15 +4,12 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
-		//初始化
 		Scanner scan =new Scanner(System.in);
-		double x,y;//定义变量
-		//扫描输入
+		double x,y;
 		System.out.println("请输入第一个操作数：X=");
 		x= scan.nextDouble();
 		System.out.println("请输入第一个操作数：Y=");
 		y= scan.nextDouble();
-		//运算输出
 		System.out.println("运行结果如下：");
 		System.out.println("x+y="+(x+y));
 		System.out.println("x-y="+(x-y));
