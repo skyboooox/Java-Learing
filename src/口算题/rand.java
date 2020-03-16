63c2589fb2366b3a9e5f835058520430005321c0
zcj924 <zcj924@gmail.com>
2020-03-16 09:51:22 +0800

    增加注释

--- src/口算题/rand.java
+++ src/口算题/rand.java
@@ -10,14 +10,14 @@ public class rand  {
 		String name = scan.next();
 		System.out.println(name+",您好,一起玩吧");
 		
-		Random random = new Random();
-		int num1 = random.nextInt(100);	
+		Random random = new Random();//创建随机数类
+		int num1 = random.nextInt(100);//生成随机数	
 		int num2 = random.nextInt(100);
 		
 		System.out.printf(" 请回答%d+%d=?\n",num1,num2);
 		int ans1 =scan.nextInt();
 			for(;ans1!=num1+num2;) {
-				System.out.printfzck("回答错误\n请重新回答%d+%d=?\n",num1,num2);
+				System.out.printf("回答错误\n请重新回答%d+%d=?\n",num1,num2);
 				ans1 =scan.nextInt();
 			}
 			System.out.printf("回答正确\n");
