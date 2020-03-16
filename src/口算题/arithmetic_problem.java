454b331c1399d992d4791c1da5d25d4207457c08
zcj924 <zcj924@gmail.com>
2020-03-16 09:49:52 +0800

    随机数

--- src/口算题/arithmetic_problem.java
+++ src/口算题/arithmetic_problem.java
@@ -1,4 +1,4 @@
-
+//package 口算题;
 import java.util.Scanner;
 public class arithmetic_problem {
 
@@ -8,6 +8,7 @@ public class arithmetic_problem {
 		System.out.println("请输入名字");
 		String name = scan.next();
 		System.out.println(name+",您好,一起玩吧");
+		
 		System.out.println("请输入第一个数：");
 		int num1 =scan.nextInt();
 		System.out.println("请输入第二个数：");
@@ -16,20 +17,12 @@ public class arithmetic_problem {
 		int num3 =scan.nextInt();
 		System.out.println(" 请回答num1+num2=?");
 		int ans1 =scan.nextInt();
-		if(ans1 == num1+num2) {
-			System.out.printf("回答正确\n");
-		}else {
-			System.out.printf("答案是:%d+%d=%d\n",num1,num2,num2+num3);
 			for(;ans1!=num1+num2;) {
-				System.out.println(" 请重新回答num1+num2=?");
+				System.out.println("回答错误\n请重新回答num1+num2=?");
 				ans1 =scan.nextInt();
 			}
-		}
-	
-		System.out.println(" 请回答num1+num2+num3=?");
-		System.out.printf("请回答:%d+%d+%d=%d\n",num1,num2,num3,num1+num2+num3);		
-		System.out.println(" 请回答num1+num2-num3=?");
-		System.out.printf("请回答:%d+%d-%d=%d\n",num1,num2,num3,num1+num2-num3);
+			System.out.printf("回答正确\n");
+
 	}
 
 }
