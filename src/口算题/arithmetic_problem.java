ec401e554ca34daecc1035d0f710b65c0db9898b
zcj924 <zcj924@gmail.com>
2020-03-16 09:15:44 +0800

    加入if

--- src/口算题/arithmetic_problem.java
+++ src/口算题/arithmetic_problem.java
@@ -1,9 +1,8 @@
-package 口算题;
+
 import java.util.Scanner;
 public class arithmetic_problem {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
 		System.out.println("游戏:口算题");
 		Scanner scan =new Scanner(System.in);
 		System.out.println("请输入名字");
@@ -16,7 +15,17 @@ public class arithmetic_problem {
 		System.out.println("请输入第三个数：");
 		int num3 =scan.nextInt();
 		System.out.println(" 请回答num1+num2=?");
-		System.out.printf("请回答:%d+%d=%d\n",num1,num2,num2+num3);
+		int ans1 =scan.nextInt();
+		if(ans1 == num1+num2) {
+			System.out.printf("回答正确\n");
+		}else {
+			System.out.printf("答案是:%d+%d=%d\n",num1,num2,num2+num3);
+			for(;ans1!=num1+num2;) {
+				System.out.println(" 请重新回答num1+num2=?");
+				ans1 =scan.nextInt();
+			}
+		}
+	
 		System.out.println(" 请回答num1+num2+num3=?");
 		System.out.printf("请回答:%d+%d+%d=%d\n",num1,num2,num3,num1+num2+num3);		
 		System.out.println(" 请回答num1+num2-num3=?");
