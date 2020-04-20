df8b5af56cceee42aada03534330fb2d809da8e0
zcj924 <zcj924@gmail.com>
2020-04-20 10:45:23 +0800

    -

---
+++ 工程文件/PROJECT_JAVA/src/PACKAGE_JAVA/TEST_CLASS_JAVA.java
@@ -0,0 +1,10 @@
+package PACKAGE_JAVA;
+
+public class TEST_CLASS_JAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 源文件src/自己名字/my_name.java
@@ -0,0 +1,8 @@
+public class my_name {
+
+	public static void main(String[] args) {
+        for(int i=0;i<100;i++)
+        System.out.println("zcj924@gmail.com");
+	}
+
+}
